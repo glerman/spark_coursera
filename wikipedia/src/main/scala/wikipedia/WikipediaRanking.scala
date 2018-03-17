f657fb8c7c66c8bda7cb2cd86bcbf74a48eb2e47
glerman <glerman@outbrain.com>
2018-03-17 22:06:22 +0200

    removed unused imports

--- wikipedia/src/main/scala/wikipedia/WikipediaRanking.scala
+++ wikipedia/src/main/scala/wikipedia/WikipediaRanking.scala
@@ -1,11 +1,7 @@
 package wikipedia
 
-import org.apache.spark.SparkConf
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
+import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.rdd.RDD
-
-import scala.collection.immutable
 
 case class WikipediaArticle(title: String, text: String) {
   /**
